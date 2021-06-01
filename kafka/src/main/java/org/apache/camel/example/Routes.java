cc569e958ae69c5aca459144b27cadc65e251ddb
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2021-06-01 16:10:47 +0200

    Managed Kafka connected with RHOAS and service binding

--- kafka/src/main/java/org/apache/camel/example/Routes.java
+++ kafka/src/main/java/org/apache/camel/example/Routes.java
@@ -1,21 +1,21 @@
 package org.apache.camel.example;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.apache.camel.builder.RouteBuilder;
 
+@ApplicationScoped
 public class Routes extends RouteBuilder {
+
     @Override
     public void configure() throws Exception {
         // produces messages to kafka
-        from("timer:foo?repeat-Count=2")
-                .routeId("FromTimer2Kafka")
-                .setBody()
-                .simple("Message #${exchangeProperty.CamelTimerCounter}")
-                .to("kafka:{{kafka.topic.name}}")
+        from("timer:foo?repeat-Count=2").routeId("FromTimer2Kafka").setBody()
+                .simple("Message #${exchangeProperty.CamelTimerCounter}").to("kafka:{{kafka.topic.name}}")
                 .log("Message correctly sent to the topic!");
 
         // kafka consumer
-        from("kafka:{{kafka.topic.name}}")
-                .to("seda:kafka-results");
+        from("kafka:{{kafka.topic.name}}").to("seda:kafka-results");
 
     }
 }
